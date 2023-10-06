2936081bc515f0ae75383fcece7ab6e471839e9b
Codie <chukwudidebest007@gmail.com>
2023-10-06 08:33:49 -0700

    Googl 25

---
+++ script.js
@@ -0,0 +1,7 @@
+// // let navbar = document.querySelector('header')
+// // let firstText = document.querySelector('.text')
+// // navbar.classList('bg-black')
+// alert('too')
+// // if (navbar == scrollY) {
+// //  alert('wow')   
+// // }
